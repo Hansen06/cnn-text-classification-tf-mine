eb1c3266305e8c06a392781941ab6e44a98a2c89
Hansen06 <haishenmuc@163.com>
2018-08-25 13:41:03 +0800

    update

---
+++ practice/cnn.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3.5
+#! encoding: utf-8
+'''
+@author: haishen
+@file: cnn.py
+@time: 18-8-25 下午12:57
+'''
+
+from tensorflow.examples.tutorials.mnist import input_data
+import tensorflow as tf
+
